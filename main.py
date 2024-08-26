3d5fac0199c710dd893bc9d59e024f04b3b73310
metantonio <antonio_martinez88@hotmail.com>
2024-08-26 11:49:13 -0400

    first push

---
+++ main.py
@@ -0,0 +1,51 @@
+import time
+import datetime
+import winsound  # Solo para Windows
+import os       # Para sistemas Unix/Linux/Mac
+
+# Configuración de la alarma
+intervalo_segundos = 5  # Intervalo de tiempo en segundos entre cada alarma
+repeticiones = 5         # Número de veces que se repetirá la alarma
+# Configurar la hora de la alarma (24 horas, formato HH:MM:SS)
+hora_objetivo = datetime.time(11, 47, 40)  # 14:30:00 (2:30 PM)
+
+
+def alarma(intervalo, repeticiones):
+    for i in range(repeticiones):
+        print(f"Alarma {i + 1}")
+        time.sleep(intervalo)
+    print("Alarma completada.")
+
+
+
+
+def reproducir_sonido():
+    # Windows
+    try:
+        winsound.Beep(1000, 1000)  # Frecuencia 1000 Hz, duración 1000 ms
+    except:
+        pass
+
+    # Unix/Linux/Mac
+    try:
+        os.system('play -nq -t alsa synth 1 sine 1000')
+    except:
+        pass
+
+def configurar_alarma(hora_objetivo, intervalo, repeticiones):
+    for i in range(repeticiones):
+        while True:
+            hora_actual = datetime.datetime.now().time()
+            if hora_actual >= hora_objetivo:
+                print(f"¡Alarma {i + 1}!")
+                reproducir_sonido()
+                break
+            time.sleep(1)
+        
+        # Espera el intervalo antes de la próxima alarma
+        hora_objetivo = (datetime.datetime.combine(datetime.date.today(), hora_objetivo) +
+                         datetime.timedelta(seconds=intervalo)).time()
+
+
+##alarma(intervalo_segundos, repeticiones)
+configurar_alarma(hora_objetivo, intervalo_segundos, repeticiones)
